f14d4040f46205301c080e2e06c0c95e9177dd4e
ptkoroma <ptkoroma@gmail.com>
2024-10-16 12:26:54 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `learn Hgov`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
