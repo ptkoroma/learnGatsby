ee68530fd274f9aa28601a7beabe984d8cee65ae
ptkoroma <ptkoroma@gmail.com>
2024-10-23 10:18:50 -0400

    add image

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,9 @@ module.exports = {
     title: `learn Hgov`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp"
+  ],
 }
